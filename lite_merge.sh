53ab5d16065ba9954a186009c6c58638a7d7f23b
aleha-druga <aleha.druga@gmail.com>
2018-09-16 21:40:08 +0500

    update merge script

--- lite_merge.sh
+++ lite_merge.sh
@@ -5,9 +5,7 @@ read tag_aosp
 echo "Enter LiteOS branch"
 read lite_branch
 
-lite_tag=$lite_branch
-aosp_tag=android-$lite_tag.0_r$tag_aosp
-list=$(grep 'remote="lite"' ./manifest/aosp/merge.xml  | awk '{print $2}' | awk -F '"' '{print $2}');
+aosp_tag=android-$lite_branch.0_r$tag_aosp
 AOSP="https://android.googlesource.com"
 LITE_PATH=$PWD
 
@@ -21,14 +19,22 @@ for merge_repos in $(grep 'remote="lite"' ./manifest/aosp/merge.xml  | awk '{pri
     fi
     git remote add aosp "${AOSP}/platform/$merge_repos"
     git fetch aosp
-    git merge $aosp_tag
+    git merge $aosp_tag --no-edit
     if [ $? -ne 0 ]; then
         echo "$merge_repos" >> ${LITE_PATH}/failed
         echo "$merge_repos failed :("
     else
-        echo "$merge_repos" >> ${LITE_PATH}/success
-        git push -u lite $lite_branch
-        git remote remove aosp
+        if [[ "$(git rev-parse HEAD)" != "$(git rev-parse lite/${lite_branch})" ]]; then
+            echo "$merge_repos" >> ${LITE_PATH}/success
+            git commit --signoff --date="$(date)" --amend --no-edit
+            echo "$merge_repos succeeded"
+            echo "Pushing!"
+            git push -u lite $lite_branch
+            git remote remove aosp
+        else
+            git remote remove aosp
+            echo "$merge_repos - unchanged"
+        fi
     fi
     cd ${LITE_PATH}
 done
